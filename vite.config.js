72813855be1b1c733f130cd072cc0ed462ebb542
Arun Dev <er.devarun@gmail.com>
2024-07-10 20:27:06 +0530

    create vercel.json file to fix routing issues on vercel & also in vite.config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  //The following codes are to fix routing issues on vercel also creates vercel.json file & done some changes.
+  server: {
+    historyApiFallback: true, //Ensure client-side routing works during development
+  },
+  build: {
+    outDir: 'dist', //Default output directory, change if necessary
+  },
 })
